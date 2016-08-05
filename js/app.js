b626e49190ef79621b861000d4f6021362017e57
Ruyu You <727809166@qq.com>
2016-08-05 11:34:03 +0800

    finish all task

--- js/app.js
+++ js/app.js
@@ -1,67 +1,60 @@
 const App = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             isEditor: true,
             elements: []
         }
     },
-
     toggle: function () {
         this.setState({
             isEditor: !this.state.isEditor
         });
     },
-
-    addElement: function (element) {
+    addElement: function(element) {
         const elements = this.state.elements;
         elements.push(element);
-        this.setState({
-            elements
-        })
+        this.setState({elements});
     },
-
-    deleteElement:function (index) {
+    deleteElement: function (index) {
         const elements = this.state.elements;
         elements.splice(index, 1);
-        this.setState({
-            elements
-        });
+        this.setState({elements});
     },
-    render: function () {
+    render: function() {
         const isEditor = this.state.isEditor;
         return <div>
-            <button onClick={this.toggle}>{isEditor ? "Preview" : "Editor"}</button>
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
             <div className={isEditor ? "" : "hidden"}>
-                <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement}/>
+                <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement} />
             </div>
             <div className={isEditor ? "hidden" : ""}>
-                <Preview elements={this.state.elements}/>
+                <Previewer elements={this.state.elements} />
             </div>
-        </div>
+        </div>;
     }
 });
 
 const Editor = React.createClass({
-    render:function () {
-        "use strict";
+    render: function () {
         return <div>
-            <Left elements={this.props.elements} onDelete={this.props.onDelete}/>
+            <Left elements={this.props.elements} onDelete={this.props.onDelete} />
             <Right onAdd={this.props.onAdd}/>
-        </div>
+        </div>;
     }
 });
 
 const Left = React.createClass({
-    remove :function (index) {
+    remove: function(index) {
         this.props.onDelete(index);
     },
-    render: function () {
-        const elements = this.props.elements.map((ele, index)=> {
+    render: function() {
+        const elements = this.props.elements.map((ele, index) => {
             return <div key={index}>
                 <input type={ele}/>
-                <button onClick={this.remove}>X</button>
-            </div>
+                <button onClick={this.remove.bind(this, index)}>X</button>
+            </div>;
         });
+
         return <div>
             {elements}
         </div>
@@ -72,22 +65,31 @@ const Right = React.createClass({
     add: function () {
         const element = $("input[name=element]:checked").val();
         this.props.onAdd(element);
+        // console.log(element)
     },
-    render: function () {
+    render: function() {
         return <div>
-            <input type="radio" name="element" value="text"/>Text
-            <input type="radio" name="element" value="date"/>Date
+            <input type="radio" name="element" value="text" />Text
+            <input type="radio" name="element" value="date" />Date
             <button onClick={this.add}>+</button>
         </div>
-
-
     }
 });
 
-const Preview = React.createClass({
+const Previewer = React.createClass({
     render: function () {
-        return <div>Preview</div>
+        const elements = this.props.elements.map((ele, index) => {
+            return <div key={index}>
+                <input type={ele}/>
+            </div>;
+        });
+        return <div>
+            {elements}
+            <button>Submit</button>
+        </div>;
     }
 });
 
-ReactDOM.render(<App/>, document.getElementById('content'));
+
+
+ReactDOM.render(<App />, document.getElementById('content'));
